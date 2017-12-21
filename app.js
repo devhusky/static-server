a296fc4b5682d5408f36182cd6829b63d8858c70
莫名 <yuxunhuang@yeah.net>
2017-12-21 16:36:17 +0800

    支持断点续传，添加文件缓存和部分格式文件压缩

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const fs = require('fs')
 const url = require('url')
 const path = require('path')
 const mime = require('mime') // 获取MIME类型
-const config = require('./config/default')
+const zlib = require('zlib') // 文件压缩
+const config = require('./config/default.js')
+const utils = require('./utils.js')
 
 
 const server = http.createServer()
 
 server.on('request', function(req, res) {
-  const pathName = url.parse(req.url).pathname
+  // http://localhost:3000/husky%E7%9A%84%E5%89%AF%E6%9C%AC.jpg
+  // 将url转码encodeURI(URI)后的字符 进行解码
+  const _url = decodeURI(req.url)
+  const pathName = url.parse(_url).pathname
   const realPath = path.normalize(config.Default.rootPath + pathName) // normalize格式化路径
   console.log('GET: ' + path.normalize(realPath));
 
@@ -28,39 +33,124 @@ server.on('request', function(req, res) {
     }
 
     if (stats.isDirectory()) { // 文件夹 列出所有文件
-      fs.readdir(realPath, function(err, files) {
-        if (err) {
-          console.log(err);
-          return
-        }
 
-        var content = `<h1>Index of ${pathName}</h1>`
-        content += `<p><a href='..'>..</a></p>`
-
-        files.forEach(function(file) {
-          const filePath = path.join(realPath, file)
-          const stat = fs.statSync(filePath)
-          if (stat && stat.isDirectory()) {
-            content += `<p><a href=${file+'/'}>${file+'/'}</p>`
-          } else {
-            content += `<p><a href=${file}>${file}</p>`
-          }
-        })
-
-        res.setHeader('Content-Type', 'text/html')
-        res.writeHead(200, 'OK')
-        res.end(content)
-      })
+      directoryResponseDeal(realPath, pathName, req, res)
     } else { // 如果不是文件夹  则预览文件
-      // 获取文件MIME类型
-      const mimeType = mime.getType(realPath)
-      res.writeHead(200, 'OK', {
-        'Content-Type': mimeType
-      })
-      fs.createReadStream(realPath).pipe(res)
+
+      fileResponseDeal(realPath, stats, req, res)
     }
   })
 })
+
+// 当前请求为文件夹的处理方法
+function directoryResponseDeal(realPath, pathName, req, res) {
+  fs.readdir(realPath, function(err, files) {
+    if (err) {
+      console.log(err);
+      return
+    }
+
+    var content = `<h1>Index of ${pathName}</h1>`
+    content += `<p><a href='..'>..</a></p>`
+
+    files.forEach(function(file) {
+      const filePath = path.join(realPath, file)
+      const stat = fs.statSync(filePath)
+      if (stat && stat.isDirectory()) {
+        content += `<p><a href=${file+'/'}>${file+'/'}</p>`
+      } else {
+        content += `<p><a href=${file}>${file}</p>`
+      }
+    })
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.writeHead(200, 'OK')
+    res.end(content)
+  })
+}
+
+// 当前请求为文件的处理方法
+function fileResponseDeal(realPath, stats, req, res) {
+  var ext = path.extname(realPath)
+  ext = ext ? ext.slice(1) : 'unknown'
+
+  // 判断文件类型是否是需要缓存的文件类型
+  if (ext.match(config.Expires.fileMatch)) {
+    /* 浏览器在发送请求之前由于检测到Cache-Control和Expires
+    （Cache-Control的优先级高于Expires，但有的浏览器不支持Cache-Control，这时采用Expires）
+    如果没有过期，则不会发送请求，而直接从缓存中读取文件。*/
+    var expires = new Date()
+    expires.setTime(expires.getTime() + config.Expires.maxAge*1000)
+    res.setHeader('Expires', expires.toUTCString())
+    res.setHeader('Cache-Control', 'max-age=' + config.Expires.maxAge)
+  }
+
+  // 读取文件最后修改时间
+  const lastModified = stats.mtime.toUTCString()
+  res.setHeader('Last-Modified', lastModified)
+
+  // 判断浏览器是否发送了 if-modified-since (修改自什么时间) 请求头信息
+  const ifModifiedSince = req.headers['if-modified-since']
+  if (ifModifiedSince && ifModifiedSince == lastModified) {
+    // 如果最后修改时间与磁盘上文件修改的时间相符合  则返回304状态
+    res.writeHead(304, 'Not Modified')
+    res.end()
+    return
+  }
+
+
+  // 判断是否满足range（断点支持）条件
+  if (req.headers['range']) {
+    const range = utils.parseRange(req.headers['range'], stats.size)
+    if (range) {
+      res.setHeader('Content-Range', 'bytes ' + range.start + '-' + range.end + '/' + stats.size)
+      res.setHeader('Content-Length', range.end - range.start + 1)
+      const options = {
+        start: range.start,
+        end: range.end
+      }
+      var raw = fs.createReadStream(realPath, options)
+      res.writeHead(206, 'Partial Content') // 局部内容
+      raw.pipe(res)
+    } else {
+      res.removeHeader('Content-Length')
+      res.writeHead(614, 'Request Range Not Satisfiable')
+      res.end()
+    }
+    return
+  }
+
+  // 创建一个可读流
+  var raw = fs.createReadStream(realPath)
+
+  // 判断是否需要压缩的文件类型
+  if (ext.match(config.Compress.zipMatch)) {
+    // gzip压缩文件
+    raw = compressHandle(raw, req, res)
+  }
+
+  // 获取文件MIME类型  注：setHeader只能写在 writeHead之前 不然会报错
+  const mimeType = mime.getType(realPath)
+  res.writeHead(200, 'OK', {
+    'Content-Type': mimeType
+  })
+
+  raw.pipe(res)
+}
+
+function compressHandle(raw, req, res) {
+  var stream = raw
+  const acceptEncoding = req.headers['accept-encoding']
+  // 判断浏览器是否支持gzip压缩
+  if (acceptEncoding.match(/\bgzip\b/)) {
+    res.setHeader('Content-Encoding', 'gzip')
+    stream = raw.pipe(zlib.createGzip())
+  } else if (acceptEncoding.match(/\bdeflate\b/)) {
+    res.setHeader('Content-Encoding', 'deflate')
+    stream = raw.pipe(zlib.createDeflate())
+  }
+  return stream
+}
 
 
 server.listen(config.Default.port, function() {
---
+++ utils.js
@@ -0,0 +1,38 @@
+// 解析断点数据区间
+exports.parseRange = function(str, size) {
+  // 暂不支持多区间 遇到多区间情况 则报416错误
+  if (str.indexOf(',') != -1) {
+    return
+  }
+  const range = str.split('-')
+  var start = parseInt(range[0], 10)
+  var end = parseInt(range[1], 10)
+
+  // 判断无效情况
+  if ((isNaN(start) && isNaN(end)) || start > end || end > size) {
+    return
+  }
+
+  /**
+    bytes=0-99，从0到99之间的数据字节。
+    bytes=-100，文件的最后100个字节。
+    bytes=100-，第100个字节开始之后的所有字节。
+    bytes=0-99,200-299，从0到99之间的数据字节和200到299之间的数据字节。
+   */
+  // 判断返回数据区间
+  if (isNaN(start)) { // -100情况
+    start = size - end
+    end = size - 1
+  } else if (isNaN(end)) { // 100-情况
+    end = size - 1
+  }
+
+  return {
+    start: start,
+    end: end
+  }
+}
+
+exports.upload = function(uploadFolder, req, res, next) {
+
+}
